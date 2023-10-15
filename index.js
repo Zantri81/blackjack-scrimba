e833d38ef8471f707f0fc7bc757d9279947dcb2c
Zantri81 <tristanzanetti@yahoo.fr>
2023-10-16 00:52:19 +0200

    first rule js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let sum = firstCard + secondCard
 
 if (sum <21) {
     console.log("Do you want to draw a new card?")
-} else if (sum === 21 ) {
+} else if (sum === 21 ) {                           //something follow = else if
     console.log("Wohoo! You've got Blackjack!")
-} else if (sum > 21) {
+} else (sum > 21) {                                 //nothing after = else
     console.log("You're out of the game!")
 }
