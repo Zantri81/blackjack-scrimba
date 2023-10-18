4cb4efd4e397d49a18a0285555ba71fbfbc91c56
Zantri81 <tristanzanetti@yahoo.fr>
2023-10-18 15:26:01 +0200

    advancements

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let firstCard = 6
-let secondCard = 9
-
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
 
-if (sum <21) {
-    console.log("Do you want to draw a new card?")
+let hasBlackJack = false
+let isAlive = true
+
+let message = ""
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21 ) {                           //something follow = else if
-    console.log("Wohoo! You've got Blackjack!")
-} else (sum > 21) {                                 //nothing after = else
-    console.log("You're out of the game!")
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+} else {                                            //nothing after = else no this : ()
+    message = "You're out of the game!"
+    isAlive = false
 }
+
+// CASH OUT!
+console.log(message)
