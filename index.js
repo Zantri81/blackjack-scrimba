19e26629ed0434bdb4e9bf4eb28933900d6d6156
Zantri81 <tristanzanetti@yahoo.fr>
2023-10-19 00:29:56 +0200

    advancement and discover of querySelector

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let firstCard = 10
+let firstCard = 6
 let secondCard = 11
 let sum = firstCard + secondCard
 
@@ -7,15 +7,19 @@ let isAlive = true
 
 let message = ""
 
+let messageEL = document.getElementById("message-el")
+let sumEL = document.querySelector("#sum-el")    //need to be more specific (CSS selector so need to be careful if it's an ID or class) (we'll see this later it's an exmemple)
+
 function startGame() {
+    sumEL.textContent += sum
 if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
+    message = "Do you want to draw a new card?"
 } else if (sum === 21 ) {                           //something follow = else if
-    message = "Wohoo! You've got Blackjack!"
+    message = "Impressive. You've got Blackjack!"
     hasBlackJack = true
 } else {                                            //nothing after = else no this : ()
     message = "You're out of the game!"
     isAlive = false
     }
-console.log(message) 
+    messageEL.textContent = message
 }
